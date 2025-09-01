1bdaf72c18fb41d33c8828490ee7d9ea3ad71fa6
Harika0306 <135519729+Harika0306@users.noreply.github.com>
2025-09-01 16:05:20 +0530

    Day 01: Added Two Sum solution

---
+++ Day01/solution.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        int left = 0, right = nums.size() - 1;
+
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum == target) {
+                return {left, right};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return {};
+    }
+};
